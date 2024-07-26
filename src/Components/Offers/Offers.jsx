764941c5472c9c5bfc87ef0d2ffa65ff3acdc5d3
Isaac Lei <isaaclei0918@gmail.com>
2024-07-26 17:07:42 +0800

    screen size responsive for files: popular, offers, newsletter, items, footer, navbar, shopcategory

--- src/Components/Offers/Offers.jsx
+++ src/Components/Offers/Offers.jsx
@@ -11,7 +11,7 @@ const Offers = () => {
         <p>ONLY ON BEST SELLERS PRODUCTS</p>
         <button> Check Now</button>
       </div>
-      <div className="offers right">
+      <div className="offers-right">
         <img src={exclusive_image} alt="" />
 
       </div>
